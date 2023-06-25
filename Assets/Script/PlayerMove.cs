865349323bdd39095f507b3b946b52b31b31d3ab
yoshimaruu <136309462+yoshimaruu@users.noreply.github.com>
2023-06-25 22:24:28 +0900

    new

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -8,6 +8,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float xMax;
     [SerializeField] private float zMax;
+    [SerializeField] private float xMin;
+    [SerializeField] private float zMin;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,7 @@ public class PlayerMove : MonoBehaviour
         {
             transform.Translate(new Vector3(moveSpeed, 0, 0) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A) && this.transform.position.x > -xMax)
+        if (Input.GetKey(KeyCode.A) && this.transform.position.x > xMin)
         {
             transform.Translate(new Vector3(-moveSpeed, 0, 0) * Time.deltaTime);
         }
@@ -30,7 +32,7 @@ public class PlayerMove : MonoBehaviour
         {
             transform.Translate(new Vector3(0, 0, moveSpeed) * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S) && this.transform.position.z > -zMax)
+        if (Input.GetKey(KeyCode.S) && this.transform.position.z > zMin)
         {
             transform.Translate(new Vector3(0, 0, -moveSpeed) * Time.deltaTime);
 
@@ -45,6 +47,7 @@ public class PlayerMove : MonoBehaviour
             Debug.Log("Enemy");
         }
     }
+
     private void OnDestroy()
     {
         SceneManager.LoadScene("End");
---
+++ Assets/Script/ScoreCount.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCount : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private float score;
+    private GameObject data;
+    private Data dataCs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = GameObject.Find("Data");
+        dataCs = data.GetComponent<Data>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = dataCs.score;
+        scoreText.text = "Score" + score.ToString();
+    }
+}
